95b87f5ea95689e9886c3b700963635cb6404abe
EdgarKol <edgar.kollamaa@vikk.ee>
2022-01-24 17:05:08 +0200

    first commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import http from 'http';
+import App from './app'
+import { openDatabaseConnection } from './db/db';
+
+
+const startServer = async () =>{
+    const httpServer = http.createServer(App)
+    await openDatabaseConnection()
+
+
+httpServer.listen(3000, () => {
+    console.log('server started on http://localhost:3000')
+})
+}
+startServer()
